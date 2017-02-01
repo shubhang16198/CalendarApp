f7c22077f47e41c2695f929d4a0b87b1c0e2b958
Shubhang Sati <shubhang.sati.1998@gmail.com>
2017-02-01 17:36:16 +0530

    files copied

---
+++ calenderApp.py
@@ -0,0 +1,221 @@
+from __future__ import print_function
+from flask import Flask, request, url_for
+from flask_pymongo import PyMongo
+import flask
+import httplib2
+import os
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+import datetime
+from collections import defaultdict
+import time
+
+
+app = Flask(__name__,static_url_path='')
+
+app.config['MONGO_DBNAME'] = 'test'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/testdb'
+app.config['SECRET_KEY'] = '0do3ITmMzXP0afu1Agg4XnGI'
+mongo = PyMongo(app)
+
+service = None
+
+@app.route('/')
+def home():
+    return app.send_static_file('html/calender.html')
+
+
+def get_start_date(startDate):
+    """
+    Given a string startDate in the format of YYYY-mm-dd
+    return a datetime object with the proper time zone
+    """
+    return (datetime.datetime
+                    .strptime(startDate, "%Y-%m-%d")
+                    .isoformat() + 'Z')
+
+def get_now_date():
+    """Return a datetime object of current time"""
+    return (datetime.datetime.utcnow()
+                    .isoformat() + 'Z')
+
+def get_calendar_list_map(service):
+    """Get the list of Calendar Names, and store their corresponding ids"""
+
+    calendarMap = defaultdict()
+    page_token = None
+
+    calendars = (service.calendarList()
+                        .list(pageToken = page_token).execute())
+
+    for calendar in calendars['items']:
+        id = calendar.get('id', None)
+        summary = calendar.get('summary', None)
+        calendarMap[summary] = id
+    return calendarMap
+
+def get_ts_from_datetime(start, end):
+    """convert datetime string into a time event
+       so we can perform time arithmetic on them"""
+
+    if start[-2:] == '00':
+        startHr = datetime.datetime.strptime(start[:-6], "%Y-%m-%dT%H:%M:%S")
+    elif start[-1] == 'Z':
+        startHr = datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%SZ")
+    else:
+        startHr = datetime.datetime.strptime(start, "%Y-%m-%d")
+
+    if end[-2:] == '00':
+        endHr = datetime.datetime.strptime(end[:-6], "%Y-%m-%dT%H:%M:%S")
+    elif end[-1] == 'Z':
+        endHr = datetime.datetime.strptime(end, "%Y-%m-%dT%H:%M:%SZ")
+    else:
+        endHr = datetime.datetime.strptime(end, "%Y-%m-%d")
+
+    startTS = time.mktime(startHr.timetuple())
+    endTS = time.mktime(endHr.timetuple())
+    return (startTS, endTS)
+
+
+def get_events_in_calendar(calendarName, calendarMap, service, startDate, endDate=None):
+    """Get all the events in a calendar for a specific time period"""
+
+    print ('\nGet ' + calendarName + ' Events Since the beginning of the year.............')
+    startDate = get_start_date(startDate)
+    endDate = get_now_date()
+    calendarId = calendarMap.get(calendarName)
+    eventsResult = (service.events().list(calendarId = calendarId, timeMin = startDate, timeMax = endDate, maxResults = 1000, singleEvents = True, orderBy = 'startTime').execute())
+
+    events = eventsResult.get('items', [])
+    events_list = []
+
+    if not events:
+        print ('No events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        event_type = calendarName
+        eventTitle = event.get('summary')
+        #event_name = ''.join([i if ord(i) < 128 else '' for i in eventTitle.replace(',','')])
+	event_id = event['id']
+	#print()
+        #print start[:10], duration, event_type, event_name
+    	mongo.db.Events.insert_one(
+        {
+	"cal_id": calendarId,
+        "_id": event_id,
+        "name":eventTitle,
+	"description":event.get('description'),
+        "start":start,
+        "end":end
+        })
+	#print("############## INSERTED ################")
+        events_list.append([event_id,start,end,event_type, eventTitle])
+    return events_list
+
+
+
+@app.route('/login')
+def login():
+  if 'credentials' not in flask.session:
+    return flask.redirect(flask.url_for('oauth2callback'))
+  credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+  if credentials.access_token_expired:
+    return flask.redirect(flask.url_for('oauth2callback'))
+  else:
+    mongo.db.Events.drop()
+    http_auth = credentials.authorize(httplib2.Http())
+    global service
+    service = discovery.build('calendar', 'v3', http = http_auth)
+    global calendarMap
+    calendarMap = get_calendar_list_map(service)
+    for calendarName in calendarMap:
+        print ("processing..." + calendarName + "..........")
+        events_list = get_events_in_calendar(calendarName, calendarMap, service, '2015-01-01')
+        #print (events_list)
+    #s = '\n'.join([calendar for calendar in calendarMap])
+    return flask.redirect(flask.url_for('home'))
+
+@app.route('/oauth2callback')
+def oauth2callback():
+  flow = client.flow_from_clientsecrets(
+      'client_secrets.json',
+      scope='https://www.googleapis.com/auth/calendar',
+      redirect_uri=flask.url_for('oauth2callback', _external=True))
+
+  if 'code' not in flask.request.args:
+    auth_uri = flow.step1_get_authorize_url()
+    return flask.redirect(auth_uri)
+  else:
+    auth_code = flask.request.args.get('code')
+    credentials = flow.step2_exchange(auth_code)
+    flask.session['credentials'] = credentials.to_json()
+    #print(credentials.to_json())
+    return flask.redirect(flask.url_for('login'))
+
+@app.route('/delete/<event_id>')
+def delete(event_id):
+	Events = mongo.db.Events.find({"_id":event_id})
+	for event in Events:
+		e=event
+	#print (e['cal_id'])
+	service.events().delete(calendarId=e['cal_id'], eventId=event_id).execute()
+	mongo.db.Events.remove({"_id":event_id})
+	return "Event Deleted"
+
+@app.route('/update/<event_id>')
+def update(event_id):
+	new_Event = {
+  		'description': None,
+		}
+	Event = mongo.db.Events.find({"_id":event_id})
+	for t in Event:
+	   e = t
+	for key in new_Event.keys():
+		e[key] = new_Event[key]
+	mongo.db.Events.save(e)
+	e['start'] = {'dateTime': e['start']}
+	e['end'] = {'dateTime':e['end']}
+	e['summary'] = e['name']
+	updated_event=service.events().update(calendarId='primary', eventId=e['_id'], body=e).execute()
+	return "Event Updated"
+
+@app.route('/all')
+def list_all():
+	Events = mongo.db.Events.find()
+	E =[]
+	for e in Events:
+		E.append(e)
+	return str(E)
+
+@app.route('/create')
+def create():
+	event = {
+  		'summary': 'Google I/O 2015',
+  		'description': 'A chance to hear more about Google\'s developer products.',
+  		'start': {
+    			'dateTime': '2017-03-28T12:29:59+05:30'
+  			 },
+  		'end': {
+    			'dateTime': '2017-03-29T12:29:59+05:30',
+  			},
+		}
+
+	event = service.events().insert(calendarId='primary', body=event).execute()
+	print(event)
+	mongo.db.Events.insert_one(
+        {
+	"cal_id": 'primary',
+        "_id":    event['id'],
+        "name":   event['summary'],
+	"description":event['description'],
+        "start":  event['start'].get('dateTime', event['start'].get('date')),
+        "end":    event['end'].get('dateTime', event['end'].get('date'))
+        })
+
+	return 'Event created: %s' % (event.get('htmlLink'))
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/calender.js
@@ -0,0 +1,230 @@
+// Default target date to today.
+
+var targetDate = new Date();
+
+// Initialize the calendar display once the page loads.
+
+window.onload = setCalendar;
+
+//----------------------------------------------------------------------------
+// This function updates the calendar display to reflect the current target
+// date.
+//----------------------------------------------------------------------------
+
+function setCalendar(event) {
+
+  var el, tableEl, rowEl, cellEl, linkEl;
+  var tmpDate, tmpDate2;
+  var i, j;
+
+  // Force a redraw by hiding the entire page.
+
+  document.body.style.display = "none";
+
+  // Update month name.
+
+  el = document.getElementById("calendarHeader").firstChild;
+  el.nodeValue = targetDate.getMonthName() + "\u00a0" + targetDate.getFullYear();
+
+  // Start with the first day of the month and go back if necessary to
+  // the previous Sunday.
+
+  tmpDate = new Date(Date.parse(targetDate));
+  tmpDate.setDate(1);
+  while (tmpDate.getDay() != 0) {
+    tmpDate.addDays(-1);
+  }
+
+  // Go through each calendar day cell in the table and update it.
+
+  tableEl = document.getElementById("calendar");
+  for (i = 2; i <= 7; i++) {
+    rowEl = tableEl.rows[i];
+
+    // Hide row if it contains no dates in the current month.
+
+    tmpDate2 = new Date(Date.parse(tmpDate));
+    tmpDate2.addDays(6);
+    if (tmpDate.getMonth()  != targetDate.getMonth() &&
+        tmpDate2.getMonth() != targetDate.getMonth())
+      rowEl.className = "empty";
+    else
+      rowEl.className = "";
+
+    // Loop through a week.
+
+    for (j = 0; j < rowEl.cells.length; j++) {
+      cellEl = rowEl.cells[j];
+      linkEl = cellEl.firstChild;
+
+      if (tmpDate.getMonth() == targetDate.getMonth()) {
+        linkEl.date = new Date(Date.parse(tmpDate));
+        s = tmpDate.toString().split(" ");
+        linkEl.title = s[0] + " " + s[1] + " " + s[2] + ", " + s[s.length - 1];
+        linkEl.firstChild.nodeValue = tmpDate.getDate();
+        linkEl.style.visibility = "";
+      }
+      else
+        linkEl.style.visibility = "hidden";
+
+      // Highlight the current date.
+
+      if (cellEl.oldClass == null)
+        cellEl.oldClass = cellEl.className;
+      if (Date.parse(tmpDate) == Date.parse(targetDate))
+        cellEl.className = cellEl.oldClass + " target";
+      else
+        cellEl.className = cellEl.oldClass;
+
+      // Go to the next day.
+
+      tmpDate.addDays(1);
+    }
+  }
+
+  // Restore the page display.
+
+  document.body.style.display = "";
+}
+
+//----------------------------------------------------------------------------
+// Event handlers for the calendar elements.
+//----------------------------------------------------------------------------
+
+function addMonths(event, n) {
+
+  // Advance the calendar month and update the display.
+
+  targetDate.addMonths(n);
+  setCalendar(event);
+}
+
+function addYears(event, n) {
+
+  // Advance the calendar year and update the display.
+
+  targetDate.addYears(n);
+  setCalendar(event);
+}
+
+function setTargetDate(event, link) {
+
+  // Change the target date and update the calendar.
+
+  if (link.date != null) {
+    targetDate = new Date(Date.parse(link.date));
+    setCalendar(event);
+  }
+}
+
+//============================================================================
+// Add new properties and methods to the Date object.
+//============================================================================
+
+// Properties
+
+Date.prototype.monthNames = new Array("January", "February", "March", "April",
+  "May", "June", "July", "August", "September", "October", "November",
+  "December");
+Date.prototype.savedDate  = null;
+
+// Methods
+
+Date.prototype.getMonthName = dateGetMonthName;
+Date.prototype.getDays      = dateGetDays;
+Date.prototype.addDays      = dateAddDays;
+Date.prototype.addMonths    = dateAddMonths;
+Date.prototype.addYears     = dateAddYears;
+
+//----------------------------------------------------------------------------
+// getMonthName(): Returns the name of the date's month.
+//----------------------------------------------------------------------------
+
+function dateGetMonthName() {
+
+  return this.monthNames[this.getMonth()];
+}
+
+//----------------------------------------------------------------------------
+// getDays(): Returns the number of days in the date's month.
+//----------------------------------------------------------------------------
+
+function dateGetDays() {
+
+  var tmpDate, d, m;
+
+  tmpDate = new Date(Date.parse(this));
+  m = tmpDate.getMonth();
+  d = 28;
+  do {
+    d++;
+    tmpDate.setDate(d);
+  } while (tmpDate.getMonth() == m);
+
+  return d - 1;
+}
+
+//----------------------------------------------------------------------------
+// addDays(n): Adds the specified number of days to the date.
+//----------------------------------------------------------------------------
+
+function dateAddDays(n) {
+
+  // Add the specified number of days.
+
+  this.setDate(this.getDate() + n);
+
+  // Reset the new day of month.
+
+  this.savedDate = this.getDate();
+}
+
+//----------------------------------------------------------------------------
+// addMonths(n): Adds the specified number of months to the date, adjusting
+// the day of the month if necessary.
+//----------------------------------------------------------------------------
+
+function dateAddMonths(n) {
+
+  // Save the day of month if not already set.
+
+  if (this.savedDate == null)
+    this.savedDate = this.getDate();
+
+  // Set the day of month to the first to avoid rolling.
+
+  this.setDate(1);
+
+  // Add the specified number of months.
+
+  this.setMonth(this.getMonth() + n);
+
+  // Restore the saved day of month, if possible.
+
+  this.setDate(Math.min(this.savedDate, this.getDays()));
+}
+
+//----------------------------------------------------------------------------
+// addYears(n): Adds the specified number of years to the date, adjusting the
+// day of the month for leap years.
+//----------------------------------------------------------------------------
+
+function dateAddYears(n) {
+
+  // Save the day of month if not already set.
+
+  if (this.savedDate == null)
+    this.savedDate = this.getDate();
+
+  // Set the day of month to the first to avoid rolling.
+
+  this.setDate(1);
+
+  // Add the specified number of years.
+
+  this.setFullYear(this.getFullYear() + n);
+
+  // Restore the saved day of month, if possible.
+
+  this.setDate(Math.min(this.savedDate, this.getDays()));
+}
